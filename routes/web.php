c5c75d64dfc3511d5b5dc152afc71f2ce84f6eb8
Elvan Hidayat <elvanhd@gmail.com>
2023-07-10 07:50:37 +0700

    view all posts by an author

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Models\Category;
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -35,3 +36,10 @@ Route::get('categories/{category:slug}', function (Category $category) {
         'categories' => Category::all()
     ]);
 })->name('category');
+
+Route::get('authors/{author:username}', function (User $author) {
+    return view('posts', [
+        'posts' => $author->posts,
+        'categories' => Category::all()
+    ]);
+});
